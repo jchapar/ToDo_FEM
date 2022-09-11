120728322a85ca80800e196023c24df41495cd41
josechaparro <josechaparro.dev@gmail.com>
2022-09-10 22:49:08 -0700

    file structure set and config done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryTextColor: 'rgb(var(--primary-text-color))',
+        secondaryTextColor: 'rgb(var(--secondary-text-color))',
+        assistingTextColor: 'rgb(var(--assisting-text-color))',
+        selectedTextColor: 'rgb(var(--selected-text-color))',
+        primaryBgColor: 'rgb(var(--primary-bg-color))',
+        listBgColor: 'rgb(var(--list-bg-color))',
+        whiteColor: 'rgb(var(--white-color))',
+      },
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
